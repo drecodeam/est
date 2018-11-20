391d09acc15b5b48457c55f0fa0ddb2951cf39e7
drecodeam <drecodeam@gmail.com>
2018-11-20 11:00:26 +0530

    fix minor bug:

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -303,7 +303,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
             task.isActive = true;
             this.currentTaskStartTime = new Date();
             task.startTime = this.currentTaskStartTime;
-            this.currentInterval = setInterval(() => this.updateTaskUI(task), 60000);
+            this.currentInterval = setInterval(() => this.updateTaskUI(task, true), 60000);
             // this.currentInterval = setInterval(() => this.updateTaskUI(task, true), 1000);
         }
     }
